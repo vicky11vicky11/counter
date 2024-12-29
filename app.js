68d410e2a0e73b8689474418b40cc4ef091fc6ca
vicky11vicky11 <vickyvignesh430329@gmail.com>
2024-12-29 16:23:50 +0530

    update all files

---
+++ app.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+document.getElementById("decreasebtn").onclick = function () {
+    count -= 1;
+    document.getElementById("myvalue").innerHTML = count;
+}
+
+document.getElementById("resetbtn").onclick = function () {
+    count = 0;
+    document.getElementById("myvalue").innerHTML = count;
+}
+
+document.getElementById("increasebtn").onclick = function () {
+    count += 1;
+    document.getElementById("myvalue").innerHTML = count;
+}
